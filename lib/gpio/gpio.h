c48d0010bd6ad0b88a49e0015b6f46a276491225
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2021-05-17 21:04:27 +0300

    gpio init moved

---
+++ lib/gpio/gpio.h
@@ -0,0 +1,52 @@
+/*********************************************************************
+
+ 1.  NAME
+     $Source$ 
+
+ 2.  DESCRIPTION
+
+ 3.  VERSIONS
+       Original:
+         $Date$ / $Lauri Vuori$
+
+       Version history:
+
+**********************************************************************/
+#ifndef GPIO_H
+#define GPIO_H
+/*-------------------------------------------------------------------*
+*    HEADER FILES                                                    *
+*--------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f429i_discovery.h"
+/*-------------------------------------------------------------------*
+*    GLOBAL VARIABLES AND CONSTANTS                                  *
+*--------------------------------------------------------------------*/
+/* Control flags */
+
+/* Global constants */
+
+/* Global variables */
+
+/* Global structures */
+/*-------------------------------------------------------------------*
+*    TASK PROTOTYPES                                                 *
+*--------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------*
+*    FUNCTION PROTOTYPES                                             *
+*--------------------------------------------------------------------*/
+extern void MX_GPIO_Init(void);
+#endif /* GPIO_H */
+
+/*** end of file ***/
+
+// /*!
+//     @struct cell
+//     @brief Structure to hold current and future tables
+//     @param current current table
+//     @param future future table
+//     @param generation adds generations every cycle
+// */ 
+
+
